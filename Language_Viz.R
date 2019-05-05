49b5478cbbe9e18e98c43fc3aa40ca80007d0463
Danny Stumpf <Danny@wirelessprv-10-193-167-177.near.illinois.edu>
2019-05-05 12:17:30 -0500

    updated graphs

--- Language_Viz.R
+++ Language_Viz.R
@@ -26,16 +26,16 @@ langauge_viz = function(filename, ngram) {
     gt_spanish_prob = ngram_prob(filename, read.csv("./STAT 385/data/es_quadgram.csv")[-1], ngram_evaluator_gt, number = 4)
   }
   
-  lap_df = data_frame("Percentage" = c(lap_english_prob, lap_italian_prob, lap_spanish_prob), "Language" = c("English", "Italian", "Spanish"))
-  gt_df = data_frame("Percentage" = c(gt_english_prob, gt_italian_prob, gt_spanish_prob), "Language" = c("English", "Italian", "Spanish"))
+  lap_df = data_frame("Prob" = c(lap_english_prob, lap_italian_prob, lap_spanish_prob), "Language" = c("English", "Italian", "Spanish"))
+  gt_df = data_frame("Prob" = c(gt_english_prob, gt_italian_prob, gt_spanish_prob), "Language" = c("English", "Italian", "Spanish"))
   
   ggplot(lap_df) +
-    aes(x = Language, y = (Percentage), fill = Language) +
-    labs(title = "Language projection using laplace smoothing") +
+    aes(x = Language, y =Prob, fill = Language) +
+    labs(title = "Language projection using laplace smoothing", subtitle = "Where the smallest ln(probability) is the least likely", y = "ln(Probability)") +
     geom_col()
 
   ggplot(gt_df) +
-    aes(x = Language, y = (Percentage), fill = Language) +
-    labs(title = "Language projection using Good Turings smoothing") +
+    aes(x = Language, y = Prob, fill = Language) +
+    labs(title = "Language projection using Good Turings smoothing", subtitle = "Where the smallest ln(probability) is the least likely", y = "ln(Probability)") +
     geom_col()
 }
